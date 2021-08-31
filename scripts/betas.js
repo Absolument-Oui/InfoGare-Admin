cf31f96c361287aad721d36c68f25c04e7858e2b
Flo Youtube 54 <48404856+FloYoutube54@users.noreply.github.com>
2021-08-31 23:16:08 +0200

    Deuxième commit à l'arrache On s'en bas les coucougnettes, y a que les admins qui s'en servent

--- scripts/betas.js
+++ scripts/betas.js
@@ -9,6 +9,20 @@ var database = firebase.database().ref('users');
             }
         });
         document.getElementById('betas_nbr').innerText = beta_lst.length;
-        document.getElementById('betas').innerText = beta_lst.toString();
+        
+        beta_lst.forEach((value, index, array) => {
+            var tr = document.createElement('tr');
+            var result = document.createElement('td');
+            var result_div = document.createElement('div');
+
+            result_div.setAttribute('class', 'cell-inner');
+            result_div.appendChild(document.createTextNode(value));
+            result.appendChild(result_div);
+
+            tr.appendChild(result);
+
+            document.getElementById('table').appendChild(tr);
+        })
+
     });
  }
--- scripts/search.js
+++ scripts/search.js
@@ -4,28 +4,26 @@ function search(query, type) {
     database.child('users').get().then((snapshot) => {
         snapshot.forEach((child) => {
             var key = child.key;
-            if (type == 'Utilisateur' || type == 'Tout') {
-                if (key.includes(query)) {
-                    console.log(key);
-                    var tr = document.createElement('tr');
-                    var result = document.createElement('td');
-                    var result_div = document.createElement('div');
-                    var type = document.createElement('tr');
-                    var type_div = document.createElement('div');
+            if (key.indexOf(query) == 0) {
+                console.log(key);
+                var tr = document.createElement('tr');
+                var result = document.createElement('td');
+                var result_div = document.createElement('div');
+                var type = document.createElement('tr');
+                var type_div = document.createElement('div');
 
-                    result_div.setAttribute('class', 'cell-inner');
-                    result_div.appendChild(document.createTextNode(key));
-                    result.appendChild(result_div);
+                result_div.setAttribute('class', 'cell-inner');
+                result_div.appendChild(document.createTextNode(key));
+                result.appendChild(result_div);
 
-                    type_div.setAttribute('class', 'cell-inner');
-                    type_div.appendChild(document.createTextNode('Utilisateur'));
-                    type.appendChild(type_div);
+                type_div.setAttribute('class', 'cell-inner');
+                type_div.appendChild(document.createTextNode('Utilisateur'));
+                type.appendChild(type_div);
 
-                    tr.appendChild(result);
-                    tr.appendChild(type);
+                tr.appendChild(result);
+                tr.appendChild(type);
 
-                    document.getElementById('cellsresults').appendChild(tr);
-                }
+                document.getElementById('cellsresults').appendChild(tr);
             }
         })
     })
