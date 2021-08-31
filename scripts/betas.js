91478cce56b00dded4eb5cdcc2e86ab6ccec7b69
Flo Youtube 54 <48404856+FloYoutube54@users.noreply.github.com>
2021-09-01 00:11:24 +0200

    Commit à l'arrache

--- scripts/betas.js
+++ scripts/betas.js
@@ -2,10 +2,12 @@ var database = firebase.database().ref('users');
 
  function loadBetas() {
     var beta_lst = new Array();
+    var uid_lst = new Array();
     database.get().then((snapshot) => {
         snapshot.forEach((child) => {
             if (child.val().beta === true) {
                 beta_lst.push(child.val().username);
+                uid_lst.push(child.key);
             }
         });
         document.getElementById('betas_nbr').innerText = beta_lst.length;
@@ -14,12 +16,19 @@ var database = firebase.database().ref('users');
             var tr = document.createElement('tr');
             var result = document.createElement('td');
             var result_div = document.createElement('div');
+            var uid = document.createElement('td');
+            var uid_div = document.createElement('div');
 
             result_div.setAttribute('class', 'cell-inner');
             result_div.appendChild(document.createTextNode(value));
             result.appendChild(result_div);
 
+            uid_div.setAttribute('class', 'cell-inner');
+            uid_div.append(document.createTextNode(uid_lst[index]));
+            uid.appendChild(uid_div);
+
             tr.appendChild(result);
+            tr.appendChild(uid);
 
             document.getElementById('table').appendChild(tr);
         })
