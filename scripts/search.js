6be8840501503ebb35c50744e23d6d538e2f92c9
Flo Youtube 54 <48404856+FloYoutube54@users.noreply.github.com>
2021-09-27 19:30:23 +0200

    Mise à jour du script de recherche

--- scripts/search.js
+++ scripts/search.js
@@ -28,31 +28,34 @@ function search(query, type) {
 
                 document.getElementById('cellsresults').appendChild(tr);
             }
-            if (child.val().username !== undefined & child.val().username.indexOf(query) == 0) {
-                var tr = document.createElement('tr');
-                var result = document.createElement('td');
-                var result_div = document.createElement('div');
-                var type = document.createElement('tr');
-                var type_div = document.createElement('div');
-
-                result_div.setAttribute('class', 'cell-inner');
-                result_div.appendChild(document.createTextNode(key));
-                result.appendChild(result_div);
-
-                type_div.setAttribute('class', 'cell-inner');
-                type_div.appendChild(document.createTextNode('Utilisateur (nom d\'utilisateur)'));
-                type.appendChild(type_div);
-
-                tr.appendChild(result);
-                tr.appendChild(type);
-
-                tr.setAttribute('cursor', 'pointer');
-                tr.setAttribute('onclick', 'location.href="user.htm?userid="'+child.key);
-
-                document.getElementById('cellsresults').appendChild(tr);
+            if (child.val().username !== undefined) {
+                if (child.val().username.indexOf(query) == 0) {
+                    console.log(key);
+                    var tr = document.createElement('tr');
+                    var result = document.createElement('td');
+                    var result_div = document.createElement('div');
+                    var type = document.createElement('tr');
+                    var type_div = document.createElement('div');
+    
+                    result_div.setAttribute('class', 'cell-inner');
+                    result_div.appendChild(document.createTextNode(key));
+                    result.appendChild(result_div);
+    
+                    type_div.setAttribute('class', 'cell-inner');
+                    type_div.appendChild(document.createTextNode('Utilisateur (nom d\'utilisateur)'));
+                    type.appendChild(type_div);
+    
+                    tr.appendChild(result);
+                    tr.appendChild(type);
+    
+                    tr.setAttribute('cursor', 'pointer');
+                    tr.setAttribute('onclick', 'location.href="user.htm?userid="'+child.key);
+    
+                    document.getElementById('cellsresults').appendChild(tr);    
+                }
             }
-        })
-    })
+        });
+    });
 }
 
 function loadSearch() {
