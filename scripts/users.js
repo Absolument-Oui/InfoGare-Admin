dad91a090ae202f7aa0464c9a94454ed483ce141
Flo Youtube 54 <48404856+FloYoutube54@users.noreply.github.com>
2021-10-04 11:26:31 +0200

    Ajout de users.htm et users.js

---
+++ scripts/users.js
@@ -0,0 +1,29 @@
+const database = firebase.database().ref();
+
+function loadUsers() {
+    database.child('users').get().then((snapshot) => {
+        snapshot.forEach((child) => {
+            var tr = document.createElement('tr');
+            var result = document.createElement('td');
+            var result_div = document.createElement('div');
+            var type = document.createElement('tr');
+            var type_div = document.createElement('div');
+
+            result_div.setAttribute('class', 'cell-inner');
+            result_div.appendChild(document.createTextNode(child.val().username));
+            result.appendChild(result_div);
+
+            type_div.setAttribute('class', 'cell-inner');
+            type_div.appendChild(document.createTextNode(child.key));
+            type.appendChild(type_div);
+
+            tr.appendChild(result);
+            tr.appendChild(type);
+
+            tr.setAttribute('cursor', 'pointer');
+            tr.setAttribute('onclick', 'location.href="user.htm?userid="'+child.key);
+
+            document.getElementById('cellsresults').appendChild(tr);    
+        })
+    })
+}
