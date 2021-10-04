06e35749b25a66cef892eeaa8126a67cd11ac226
MaxALEau <varymaxence@gmail.com>
2021-10-04 10:20:57 +0100

    Ajout de la photo de profil (#1)
    * Ajout de la photo de profil
    
    * js

--- scripts/profile.js
+++ scripts/profile.js
@@ -32,6 +32,11 @@ document.getElementById('login').onclick = function() {
         document.getElementById('login').hidden = true;
         document.getElementById('username').innerText = user.displayName;
         document.getElementById('logout').hidden = false;
+        if (user.photoURL !== undefined) {
+          document.getElementById('mnu_user_photo').src = user.photoURL;
+          document.getElementById('mnu_user_photo').style.display = 'block';
+          document.getElementById('mnu_user_no_photo').style.display = 'none';
+        }
         this.user = user;
       } else {
         document.getElementById('login').hidden = false;
