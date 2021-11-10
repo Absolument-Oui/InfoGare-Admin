859395b9b8f251faf4b7d53e3ecafcb4f7b21740
Flo Youtube 54 <48404856+FloYoutube54@users.noreply.github.com>
2021-11-10 20:04:11 +0100

    Mise à jour de la page user.htm et du script user.js

--- scripts/user.js
+++ scripts/user.js
@@ -73,6 +73,8 @@ function getUserGares(user) {
     })
 }
 
-function loginAsUser(uid) {
+function loginAsUser() {
+    var params = new URLSearchParams(location.search);
+    var uid = params.get('userid');
     location.href = `https://auth.infogare.fr/signin_uid.htm?uid=${uid}&returnurl=https%3A%2F%2Finfogare.fr`;
 }
